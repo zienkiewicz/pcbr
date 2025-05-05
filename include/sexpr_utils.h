e1f4f57306d8263ff06ec196dac18075ce5b7c93
Kacper Zienkiewicz <k09028449@gmail.com>
2025-05-05 17:54:34 +0200

    sexpr_utils: Add regex variants for s-expr traverse functions

--- include/sexpr_utils.h
+++ include/sexpr_utils.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "sexpr.h"
 #include <vector>
+#include <regex>
 
 const SEXPR::SEXPR_LIST *find_sub_sexpr(const SEXPR::SEXPR_LIST *sexpr, const std::string& key);
 void find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::string& key, std::vector<const SEXPR::SEXPR_LIST*>& results);
 std::vector<const SEXPR::SEXPR_LIST*> find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::string& key);
+
+// Regex variants
+void find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::regex& pattern, std::vector<const SEXPR::SEXPR_LIST*>& results);
+std::vector<const SEXPR::SEXPR_LIST*> find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::regex& pattern);
+
--- src/sexpr_utils.cpp
+++ src/sexpr_utils.cpp
@@ -1,6 +1,7 @@
 #include "sexpr.h"
 #include <vector>
 #include <string>
+#include <regex>
 
 const SEXPR::SEXPR_LIST *find_sub_sexpr(const SEXPR::SEXPR_LIST *sexpr, const std::string& key) {
         for (int64_t i = 0; i < sexpr->GetNumberOfChildren(); i++) {
@@ -27,6 +28,7 @@ const SEXPR::SEXPR_LIST *find_sub_sexpr(const SEXPR::SEXPR_LIST *sexpr, const st
         return nullptr;
 }
 
+// TODO: Fix glaring code duplication
 void find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::string& key, std::vector<const SEXPR::SEXPR_LIST*>& results) {
     for (int64_t i = 0; i < sexpr->GetNumberOfChildren(); i++) {
         SEXPR::SEXPR* child = sexpr->GetChild(i);
@@ -52,5 +54,33 @@ void find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::string& key,
 std::vector<const SEXPR::SEXPR_LIST*> find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::string& key) {
     std::vector<const SEXPR::SEXPR_LIST*> results;
     find_all_sub_sexprs(sexpr, key, results);
+    return results;
+}
+
+void find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::regex& pattern, std::vector<const SEXPR::SEXPR_LIST*>& results) {
+    for (int64_t i = 0; i < sexpr->GetNumberOfChildren(); i++) {
+        SEXPR::SEXPR* child = sexpr->GetChild(i);
+        if (!child->IsList()) {
+            continue;
+        }
+
+        const SEXPR::SEXPR_LIST* list = child->GetList();
+        size_t count = list->GetNumberOfChildren();
+        if (count < 1) {
+            continue;
+        }
+
+        SEXPR::SEXPR* head = list->GetChild(0);
+        if (head->IsSymbol() && std::regex_match(head->GetSymbol(), pattern)) {
+            results.push_back(list);
+        }
+
+        find_all_sub_sexprs(list, pattern, results);
+    }
+}
+
+std::vector<const SEXPR::SEXPR_LIST*> find_all_sub_sexprs(const SEXPR::SEXPR_LIST* sexpr, const std::regex& pattern) {
+    std::vector<const SEXPR::SEXPR_LIST*> results;
+    find_all_sub_sexprs(sexpr, pattern, results);
     return results;
 }
